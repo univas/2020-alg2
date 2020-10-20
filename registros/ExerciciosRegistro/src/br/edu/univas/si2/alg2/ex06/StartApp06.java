f7b0dfafb3381c0849377d67835844aa5b2213a5
Roberto Rocha <rrocha.roberto@gmail.com>
2020-10-20 20:08:37 -0300

    Início do exercício 06 da lista 3.

---
+++ registros/ExerciciosRegistro/src/br/edu/univas/si2/alg2/ex06/StartApp06.java
@@ -0,0 +1,15 @@
+package br.edu.univas.si2.alg2.ex06;
+
+public class StartApp06 {
+
+	public static void main(String[] args) {
+		
+	}
+	
+	public static boolean dentroDoCirculo(Ponto centro, int raio, Ponto p) {
+		
+		//TODO: implementar a lógica
+		
+		return false;
+	}
+}
