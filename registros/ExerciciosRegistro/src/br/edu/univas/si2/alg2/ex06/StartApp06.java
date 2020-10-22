a4ec43345a248f6a9acda6a2ecd68e5919144f2a
Roberto Rocha <rrocha.roberto@gmail.com>
2020-10-22 20:25:22 -0300

    Correção do exercício 06.

--- registros/ExerciciosRegistro/src/br/edu/univas/si2/alg2/ex06/StartApp06.java
+++ registros/ExerciciosRegistro/src/br/edu/univas/si2/alg2/ex06/StartApp06.java
@@ -9,15 +9,15 @@ public class StartApp06 {
 		
 		int raio = 2;
 		
-		Ponto p1 = new Ponto();
+		Ponto p1 = new Ponto(); //ponto E
 		p1.x = 1;
 		p1.y = 1;
 		
-		Ponto p2 = new Ponto();
+		Ponto p2 = new Ponto(); //ponto C
 		p2.x = 4;
 		p2.y = 2;
 		
-		Ponto p3 = new Ponto();
+		Ponto p3 = new Ponto(); //ponto D
 		p3.x = 5;
 		p3.y = 4;
 		
@@ -26,13 +26,26 @@ public class StartApp06 {
 		System.out.println("Ponto 3: " + dentroDoCirculo(centro, raio, p3));
 	}
 	
-	public static boolean dentroDoCirculo(Ponto centro, int raio, Ponto p) {
+	public static boolean dentroDoCirculo(Ponto a, int raio, Ponto b) {
 		
-		//TODO: implementar a lógica
-		int valor = 0;
+		System.out.println("dentro do método: dentroDoCirculo: raio: " + raio);
+		//=raiz((xb - xa)^2  + (yb - ya)^2)
+		//centro = pontoA
+		//p = pontoB
+		
+		int parte1 =  b.x - a.x;
+		int parte2 =  b.y - a.y;
+		
+		int valor = (parte1 * parte1 + parte2 * parte2);
+		
+		System.out.println("valor: " + valor);
+		
+		//valor = Math.abs(valor); //módulo
 		
 		double resultadoDaRaiz = Math.sqrt(valor);
 		
-		return false;
+		System.out.println("Raiz de " + valor + ": " + resultadoDaRaiz);
+		
+		return resultadoDaRaiz <= raio;
 	}
 }
